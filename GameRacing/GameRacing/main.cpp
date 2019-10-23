079dc6944a92752863ff7a0ae1fd7202de0da47f
BlackBurn <BlackBurn@DESKTOP-3SB31V4>
2019-10-24 01:32:17 +0200

    Dodano okno ustawienie klatek/s

---
+++ GameRacing/GameRacing/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+
+#include <SDL.h>
+#include <SDL_image.h>
+
+
+
+using namespace std;
+
+#include "fps.cpp"
+
+SDL_Window* okno;
+SDL_Surface* ekran;
+SDL_Event zdarzenie;
+SDL_Rect rect1, rect2;
+
+
+int main(int argc, char* args[])
+{
+	okno = SDL_CreateWindow("Gra", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
+	ekran = SDL_GetWindowSurface(okno);
+
+	klasa_fps fps;
+
+	while (true)
+	{
+		fps.start();
+		while (SDL_PollEvent(&zdarzenie))
+		{
+			if (zdarzenie.type == SDL_QUIT)
+			{
+				exit(0);
+			}
+		}
+
+		SDL_UpdateWindowSurface(okno);
+
+		fps.end();
+	}
+}
+
