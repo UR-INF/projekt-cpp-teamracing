b1c442e47209d1198ad346436d3182c4bf17926f
DemoBoss <lukasz3287@o2.pl>
2019-11-11 12:27:58 +0100

    dodanie okna z trasą i auta

--- GameRacing/GameRacing/main.cpp
+++ GameRacing/GameRacing/main.cpp
@@ -3,8 +3,6 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
-
-
 using namespace std;
 
 #include "fps.cpp"
@@ -13,18 +11,29 @@ SDL_Window* okno;
 SDL_Surface* ekran;
 SDL_Event zdarzenie;
 SDL_Rect rect1, rect2;
+const Uint8* keystate;
 
 
 int main(int argc, char* args[])
 {
-	okno = SDL_CreateWindow("Gra", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
+	okno = SDL_CreateWindow("Gra", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
 	ekran = SDL_GetWindowSurface(okno);
 
 	klasa_fps fps;
+	SDL_Surface * droga = IMG_Load("doga.png");
+	SDL_Surface * postac = IMG_Load("postac.png");
+
+	int klatka = 0;
+
+	float posY = 360;
+	float velY = 0;
 
 	while (true)
 	{
 		fps.start();
+		klatka++;
+		keystate = SDL_GetKeyboardState(NULL);
+
 		while (SDL_PollEvent(&zdarzenie))
 		{
 			if (zdarzenie.type == SDL_QUIT)
@@ -33,8 +42,32 @@ int main(int argc, char* args[])
 			}
 		}
 
-		SDL_UpdateWindowSurface(okno);
+		{//fizyka
+			posY = posY + velY;
+		}
 
+		{//render
+			{//t³o
+				SDL_FillRect(ekran, NULL, NULL);
+			}
+
+			{//droga
+				for (int loop = 0; loop < 8; loop++) {
+
+					rect2.x = loop * 200 - (klatka * 8) % 200;
+					rect2.y = 0;
+					SDL_BlitSurface(droga, NULL, ekran, &rect2);
+				}
+			}
+			{//postac
+				rect2.x = 100;
+				rect2.y = posY - 20;
+				SDL_BlitSurface(postac, NULL, ekran, &rect2);
+			}
+			
+		}
+
+		SDL_UpdateWindowSurface(okno);
 		fps.end();
 	}
 }
