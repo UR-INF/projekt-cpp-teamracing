094aa3b146e41a1445b2f22384bccbaf6edf9759
DemoBoss <lukasz3287@o2.pl>
2019-11-22 14:47:28 +0100

    sterowanie autka i render przez karte graficzną

--- GameRacing/GameRacing/main.cpp
+++ GameRacing/GameRacing/main.cpp
@@ -6,22 +6,47 @@
 using namespace std;
 
 #include "fps.cpp"
+#include "klasy.cpp"
+#include <string>
 
 SDL_Window * okno;
-SDL_Surface* ekran;
+//SDL_Surface* ekran;
+SDL_Renderer* render;
 SDL_Event zdarzenie;
 SDL_Rect rect1, rect2;
+
 const Uint8* keystate;
+
+SDL_Texture* loadTexture(std::string path) {
+
+	SDL_Texture* newTexture = NULL;
+	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+	if (loadedSurface == NULL) {
+
+		printf("Nie załadowano obrazu %s  SDL_image error: %s\n", path.c_str(), IMG_GetError());
+	}
+	else
+	{
+		newTexture = SDL_CreateTextureFromSurface(render, loadedSurface);
+		if (newTexture == NULL) {
+			printf("Nie załadowano tekstur %s  SDL error: %s\n", path.c_str(), SDL_GetError());
+		}
+		SDL_FreeSurface(loadedSurface);
+	}	
+	return newTexture;
+}
 
 
 int main(int argc, char* args[])
 {
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+
 	okno = SDL_CreateWindow("Gra", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
-	ekran = SDL_GetWindowSurface(okno);
+	render = SDL_CreateRenderer(okno, -1, SDL_RENDERER_ACCELERATED);
 
 	klasa_fps fps;
-	SDL_Surface* droga = IMG_Load("droga.bmp");
-	SDL_Surface* postac = IMG_Load("postac.bmp");
+	SDL_Texture* droga = loadTexture("droga.bmp");
+	SDL_Texture* postac = loadTexture("postac.bmp");
 
 	int klatka = 0;
 
@@ -42,32 +67,65 @@ int main(int argc, char* args[])
 			}
 		}
 
+		{//sterowanie
+
+			if (keystate[SDL_SCANCODE_DOWN]) {
+				if(velY < 25){
+					velY = velY + 0.6;
+				}
+			}
+			if (keystate[SDL_SCANCODE_UP]) {
+				if(velY > -25){
+					velY = velY - 0.6;
+				}
+			}
+			velY = velY * 0.9;
+
+		}
+
+
 	{//fizyka
 		posY = posY + velY;
+
+		if (posY < 100) {
+			velY = velY + 1;
+			velY = velY * 0.9;
+
+		}
+		if (posY > 620) {
+			velY = velY - 1;
+			velY = velY * 0.9;
+
+		}
 	}
 
 	{//render
 		{//tlo
-			SDL_FillRect(ekran, NULL, NULL);
+			SDL_RenderClear(render);
 		}
 
 		{//droga
 			for (int loop = 0; loop < 8; loop++) {
 
-				rect2.x = loop * 200 - (klatka * 8) % 200;
+				rect2.x = loop * 200 - (klatka * 6) % 200;
 				rect2.y = 0;
-				SDL_BlitSurface(droga, NULL, ekran, &rect2);
+				rect2.w = 200;
+				rect2.h = 720;
+				SDL_RenderCopy(render, droga, NULL, &rect2);
 			}
 		}
 		{//postac
 			rect2.x = 100;
 			rect2.y = posY - 20;
-			SDL_BlitSurface(postac, NULL, ekran, &rect2);
+			rect2.w = 80;
+			rect2.h = 40;
+
+			SDL_RenderCopyEx(render,postac,NULL,&rect2,velY*8,NULL,SDL_FLIP_NONE);
 		}
 
 	}
 
-	SDL_UpdateWindowSurface(okno);
+	SDL_RenderPresent(render);
 	fps.end();
 }
 }
